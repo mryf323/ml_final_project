1b43f6fff591822fe923fd0b25dcefd891d500e6
mryf <yazdanifar@ut.ac.ir>
2021-07-23 16:34:57 +0430

    K-Fold Validation added. MLP added.

--- text_processors/stemming.py
+++ text_processors/stemming.py
@@ -1,10 +1,12 @@
 import nltk
+import numpy as np
+from nltk.tokenize import word_tokenize
 from text_processors.text_processor import TextProcessor
 
 
 class StemmingProcessor(TextProcessor):
+    stemmer = nltk.porter.PorterStemmer()
 
     def process(self, text):
-        stemmer = nltk.porter.PorterStemmer()
-        text = ' '.join([stemmer.stem(word) for word in text.split()])
+        text = ' '.join([StemmingProcessor.stemmer.stem(word) for word in text.split()])
         return TextProcessor.process(self, text)
