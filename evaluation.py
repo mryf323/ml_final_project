4a33f7f19f2966615af1315f459fa47bc8e60284
mryf <yazdanifar@ut.ac.ir>
2021-07-27 11:09:55 +0430

    Phase 2 experiments completed.

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,6 @@
 import sklearn.metrics
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, adjusted_rand_score, \
+    adjusted_mutual_info_score
 
 
 def analysis(labels, predictions):
@@ -7,6 +8,13 @@ def analysis(labels, predictions):
     print("Matrix: Confusion\n", confusion_matrix(labels, predictions))
     print("Accuracy:\n", accuracy_score(labels, predictions))
     return f1_score(labels, predictions)
+
+
+def cluster_analysis(labels, predictions):
+    print('Test Evaluation:')
+    print(f'adjusted_rand_score: {adjusted_rand_score(labels, predictions)}')
+    print(f'adjusted_mutual_info_score: {adjusted_mutual_info_score(labels, predictions)}')
+
 
 def evaluate_models_with_data(models, X_train, X_test, Y_train, Y_test):
     for name, model in models.items():
